48e8ecc5a86988ee992e5bfe574542cf79ad8dd5
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-03-27 13:37:05 +0100

    feat(models): add User model

---
+++ src/models/sequelize/User.ts
@@ -0,0 +1,29 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import sequelize from '../../db';
+import { UUID } from 'crypto';
+
+interface IUser extends Model<InferAttributes<IUser>, InferCreationAttributes<IUser>> {
+  id: UUID;
+  username: string;
+  password: string;
+}
+
+const User = sequelize.define<IUser>('User', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+User.sync();
+
+export default User;
