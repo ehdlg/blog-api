4bf3bfe4cdebb51656908bf267569cfcc3e5bcd6
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-03-31 09:05:33 +0200

    feat(model): add User model

---
+++ src/models/User.model.ts
@@ -0,0 +1,49 @@
+import { User } from '../db';
+
+export default class UserModel {
+  static async get({ username }: { username?: string }) {
+    if (null == username) return await User.findAll();
+
+    const newUser = await User.findOne({ where: { username: username } });
+
+    return newUser;
+  }
+
+  static async create({ username, password }: { username: string; password: string }) {
+    try {
+      const newUser = await User.create({ username, password, id: crypto.randomUUID() });
+
+      return { data: newUser, message: 'User created succesully' };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async delete({ username }: { username: string }) {
+    try {
+      const deletedUser = await User.destroy({ where: { username } });
+
+      const success = deletedUser === 1;
+
+      const message = `User ${username} ${success ? 'deleted' : 'not found'}`;
+
+      return { data: deletedUser, message };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async update({
+    username,
+    updatedUser,
+  }: {
+    username: string;
+    updatedUser: { username: string; password: string };
+  }) {
+    try {
+      return await User.update(updatedUser, { where: { username } });
+    } catch (e) {
+      throw e;
+    }
+  }
+}
