65e4be8ec4db2be9a9d3ac28f0db9727402a8f64
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-01 15:12:40 +0200

    feat(routes): add User routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,24 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
+import UserModel from '../models/User.model';
+import { errorHandler, generateHashedPassword } from '../middlewares';
+import { createUserRules, updateUserRules, validation } from '../middlewares/validation';
+import { User } from '../db';
+import UserController from '../controllers/User.controller';
 
 const router = Router();
 
 router.get('/', (_, res) => {
   res.json({ message: 'Welcome to the Blog API' });
 });
+
+router.get('/user', UserController.get);
+
+router.post('/user', createUserRules, validation, generateHashedPassword, UserController.create);
+
+router.delete('/user', UserController.delete);
+
+router.patch('/user', updateUserRules, validation, UserController.update);
+
+router.use(errorHandler);
 
 export default router;
