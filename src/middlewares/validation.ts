c7eda182d2dfed8b85c90689b27ce95dd79706e7
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-01 15:10:22 +0200

    feat(validation): split user rules and functions in create and update

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,26 +1,45 @@
-import { body, validationResult, ValidationChain } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 import { RequestHandler } from 'express';
 
-export const userValidationRules = (isUpdate: boolean) => {
-  const rules: ValidationChain[] = [
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/;
+
+export const createUserRules = (() => {
+  return [
     body('username')
-      .notEmpty()
+      .exists()
+      .withMessage('Username required')
       .isLength({ min: 3, max: 15 })
       .withMessage('Username must be between 3 and 15 characters long.'),
     body('password')
+      .exists()
+      .withMessage('Password required')
       .isLength({ min: 8 })
       .withMessage('Password must be at least 8 characters long.')
-      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/)
+      .matches(PASSWORD_REGEX)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, one number, and one of the following special characters: !@#$%^&*'
       ),
   ];
+})();
 
-  if (isUpdate) {
-    rules.forEach((rule) => rule.optional());
-  }
-  return rules;
-};
+export const updateUserRules = (() => {
+  return [
+    body('username').optional(),
+    body('updatedUser').exists().withMessage('You must send the updated fields'),
+    body('updatedUser.username')
+      .optional()
+      .isLength({ min: 3, max: 15 })
+      .withMessage('Username must be between 3 and 15 characters long.'),
+    body('updatedUser.password')
+      .optional()
+      .isLength({ min: 8 })
+      .withMessage('Password must be at least 8 characters long.')
+      .matches(PASSWORD_REGEX)
+      .withMessage(
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one of the following special characters: !@#$%^&*'
+      ),
+  ];
+})();
 
 export const validation: RequestHandler = (req, res, next) => {
   const errors = validationResult(req);
