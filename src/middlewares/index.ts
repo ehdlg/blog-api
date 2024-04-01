59ff91ea549f7106be2d1f4f14ecbf5696be73a1
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-01 15:09:45 +0200

    feat(middleware): update errorHandler middelware and add notFoundHandler middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,8 @@
-import { ErrorRequestHandler, RequestHandler } from 'express';
-import { ValidationChain, body, validationResult } from 'express-validator';
+import { ErrorRequestHandler, NextFunction, Request, RequestHandler, Response } from 'express';
 import bcrypt from 'bcrypt';
-import UserModel from '../models/User.model';
 
 export const generateHashedPassword: RequestHandler = async (req, res, next) => {
   const { password }: { password: string } = req.body;
-
-  if (!password) throw new Error('Password must not be empty');
 
   try {
     const SALT = 10;
@@ -15,13 +11,28 @@ export const generateHashedPassword: RequestHandler = async (req, res, next) => 
     res.locals.hashedPassword = hashedPassword;
 
     next();
-  } catch (e) {
-    return res.status(500).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 };
 
-export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+export const notFoundHandler: RequestHandler = async (_, __, next) => {
+  const error = {
+    status: 404,
+    message: 'Not found!',
+  };
+
+  next(error);
+};
+
+export const errorHandler: ErrorRequestHandler = (
+  error: any,
+  _: Request,
+  res: Response,
+  __: NextFunction
+) => {
   const status = error.status || 500;
+
   const message = error.message || 'Something went wrong.';
 
   return res.status(status).json({ error: message });
