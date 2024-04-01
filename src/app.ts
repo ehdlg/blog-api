14f2a74590bc63142d25a637ebe8f4561f2e812c
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-01 08:03:46 +0200

    feat(middleware): add errorHandler  middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiRoute from './routes/api';
 import 'dotenv/config';
+import { errorHandler } from './middlewares';
 
 const app = express();
 const { PORT } = process.env;
@@ -12,6 +13,8 @@ app.use('/api', apiRoute);
 app.use((_, res) => {
   res.status(404).json({ message: 'Not found' });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on: http://localhost:${PORT}`);
