f827d48568cea5d8c361d26ef936a83502f3edbc
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-01 15:11:10 +0200

    feat(db): create and use initDb function that checks the DB connection

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
 import express from 'express';
 import apiRoute from './routes/api';
 import 'dotenv/config';
-import { errorHandler } from './middlewares';
+import { errorHandler, notFoundHandler } from './middlewares';
+import { initDb } from './db';
 
 const app = express();
 const { PORT } = process.env;
+
+function main() {
+  try {
+    initDb();
+    console.log(`App listening on: http://localhost:${PORT}`);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 app.use(express.json());
 
 app.use('/api', apiRoute);
 
-app.use((_, res) => {
-  res.status(404).json({ message: 'Not found' });
-});
+app.use(notFoundHandler);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`App listening on: http://localhost:${PORT}`);
-});
+app.listen(PORT, main);
--- src/db/index.ts
+++ src/db/index.ts
@@ -4,18 +4,20 @@ import Post from './sequelize/Post';
 import sequelize from './sequelize/config';
 import 'dotenv/config';
 
-Post.belongsTo(User, { foreignKey: 'user_id' });
-User.hasMany(Post, { foreignKey: 'user_id' });
-Comment.belongsTo(Post, { foreignKey: 'post_id' });
-Post.hasMany(Comment, { foreignKey: 'post_id' });
+export const initDb = () => {
+  Post.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasMany(Post, { foreignKey: 'user_id' });
+  Comment.belongsTo(Post, { foreignKey: 'post_id' });
+  Post.hasMany(Comment, { foreignKey: 'post_id' });
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log('Tables synced');
-  })
-  .catch((e) => {
-    console.error('Error while trying to sync the tables ', e);
-  });
+  sequelize
+    .sync()
+    .then(() => {
+      console.log('Tables synced');
+    })
+    .catch((e) => {
+      console.error('Error while trying to sync the tables ', e);
+    });
+};
 
 export { User, Post, Comment };
