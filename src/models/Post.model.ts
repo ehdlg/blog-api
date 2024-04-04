2f816e9c5e16479499c5e311ef1070123f7cff79
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-04 08:45:22 +0200

    feat(model): add Post model

---
+++ src/models/Post.model.ts
@@ -0,0 +1,50 @@
+import { Post } from '../db';
+import { Op, where } from 'sequelize';
+import { CreatePost, UpdatePost, DeletePost, GetPost } from './types';
+
+export default class PostModel {
+  static async get({ id, title }: GetPost) {
+    try {
+      if (null == id && null == title) return await Post.findAll();
+
+      if (null !== id) return await Post.findOne({ where: { id } });
+
+      return await Post.findAll({
+        where: {
+          title: {
+            [Op.like]: `${title}%`,
+          },
+        },
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async create({ id, title, content, user_id }: CreatePost) {
+    try {
+      return await Post.create({ id, title, content, user_id });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async delete({ id }: DeletePost) {
+    try {
+      return await Post.destroy({ where: { id } });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async update({ id, title, content, likes, views, updated_at, published_at }: UpdatePost) {
+    try {
+      return await Post.update(
+        { title, content, likes, views, updated_at, published_at },
+        { where: { id } }
+      );
+    } catch (e) {
+      throw e;
+    }
+  }
+}
