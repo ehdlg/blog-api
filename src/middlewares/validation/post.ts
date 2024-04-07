049fa2f9c4a53aacda90d4c281f361e3a92cea94
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-07 09:16:44 +0200

    feat(validation): add post validation for get, update and create

--- src/middlewares/validation/post.ts
+++ src/middlewares/validation/post.ts
@@ -4,13 +4,16 @@ import { UUID } from 'crypto';
 import { userExists } from './user';
 
 const TITLE_LENGTH = {
-  min: 5, 
-  max: 255
-}
-
+  min: 5,
+  max: 255,
+};
 const CONTENT_LENGTH = {
-  min: 50
-}
+  min: 50,
+};
+const CONTENT_LENGHT_MESSAGE = `Content must have at least ${CONTENT_LENGTH.min} characters long.`;
+const TITLE_LENGTH_MESSAGE = `The title must be between ${TITLE_LENGTH.min} and ${TITLE_LENGTH.max} characters long`;
+const dateTimeRegex =
+  /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]) (?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)\.(\d{6})$/;
 
 const postExists = async (id: UUID) => {
   const post = await PostModel.get({ id });
@@ -18,17 +21,21 @@ const postExists = async (id: UUID) => {
   if (null == post) throw new Error(`Post with id:${id} not found.`);
 };
 
-const createPostRules = [
+export const getPostRules = [
+  param('id').exists().isUUID().withMessage('Invalid Post ID').custom(postExists),
+];
+
+export const createPostRules = [
   body('title')
     .exists()
     .withMessage('The post must have a title')
     .isLength(TITLE_LENGTH)
-    .withMessage('The title must be between 5 and 255 characters long'),
+    .withMessage(TITLE_LENGTH_MESSAGE),
   body('content')
     .exists()
     .withMessage('The post must have a content')
     .isLength(CONTENT_LENGTH)
-    .withMessage(`Content must have at least ${CONTENT_LENGTH.min} characters long.`),
+    .withMessage(CONTENT_LENGHT_MESSAGE),
   body('user_id')
     .exists()
     .withMessage('You must provide a user ID')
@@ -37,8 +44,20 @@ const createPostRules = [
     .custom(userExists),
 ];
 
-updatePostRules = [param('id').exists().isUUID().withMessage('Invalid Post ID').custom(postExists),
-  body('title').
+export const updatePostRules = [
+  param('id').exists().isUUID().withMessage('Invalid Post ID').custom(postExists),
+  body('content').optional().isLength(CONTENT_LENGTH).withMessage(CONTENT_LENGHT_MESSAGE),
+  body('title').optional().isLength(TITLE_LENGTH).withMessage(TITLE_LENGTH_MESSAGE),
+  body('likes').optional().isInt({ min: 0 }).withMessage('Likes must be an integer greater than 0'),
+  body('views').optional().isInt({ min: 0 }).withMessage('Views must be an integer greater than 0'),
+  ,
+  body('published_at')
+    .optional()
+    .matches(dateTimeRegex)
+    .withMessage('The date format should be: YYYY-MM-DD HH:MM:SS'),
+  body('updated_at')
+    .optional()
+    .matches(dateTimeRegex)
+    .withMessage('The date format should be: YYYY-MM-DD HH:MM:SS'),
+  body('image_url').optional(),
 ];
-
-const updatePostRules = [param('id')];
